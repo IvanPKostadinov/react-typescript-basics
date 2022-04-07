62e2b3e9cb324a00d61990a4f6f0bcc62d324b67
IvanPKostadinov <ikostadinov@melontech.com>
2022-04-07 16:32:01 +0300

    added className=... with Styles; added onRemoveTodo function to handle removing a Todo element

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 
 import Todo from '../models/todo';
 import TodoItem from './TodoItem';
+import classes from './Todos.module.css';
 
 // function Todos(props: { items: string[], children: any }) {
 // React.FC<{ items: string [] } -> props: { children: any, items string [] }
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         // <li key={item.id}>{item.text}</li>
         // !!! we can add key={...} even though we don't specify it in TodoItem.tsx
         // because it is of type React.FC !!!
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          // .bind(what-this-refers-to, first-argument-the-function-will-receive-later)
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
