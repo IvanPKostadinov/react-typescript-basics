6325d0b2a2b98449827fe88a0037ba0ee0384b74
IvanPKostadinov <ikostadinov@melontech.com>
2022-04-07 11:52:30 +0300

    small fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-<<<<<<< HEAD
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-ReactDOM.render(
-  // <React.StrictMode>
-    <App />,
-  // </React.StrictMode>,
-  document.getElementById('root')
-);
-
->>>>>>> f7b7df87e7b374bb1df01618ada27282f735d6dc
