6d304b5295fd4faf6dc4526ccbe95fb91e3a28e4
IvanPKostadinov <ikostadinov@melontech.com>
2022-04-07 13:09:03 +0300

    added Type to Component's props with React.FC<...>

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,3 +1,15 @@
-function Todos() {};
+import React from 'react';
+
+// function Todos(props: { items: string[], children: any }) {
+// React.FC<{ items: string [] } -> props: { children: any, items string [] }
+const Todos: React.FC<{ items: string[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default Todos;
