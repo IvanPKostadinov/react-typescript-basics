4357e08304525b670650a972298901b6e3662169
IvanPKostadinov <ikostadinov@melontech.com>
2022-04-07 13:26:14 +0300

    added a Data Model - class Todo, which I used as a type in Todos.tsx as well

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import Todo from '../models/todo';
+
 // function Todos(props: { items: string[], children: any }) {
 // React.FC<{ items: string [] } -> props: { children: any, items string [] }
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
