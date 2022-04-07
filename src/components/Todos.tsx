fb3b6187f9750b3278bf7e77e9ac10eb321a2300
IvanPKostadinov <ikostadinov@melontech.com>
2022-04-07 13:34:29 +0300

    added TodoItem using TS

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Todo from '../models/todo';
+import TodoItem from './TodoItem';
 
 // function Todos(props: { items: string[], children: any }) {
 // React.FC<{ items: string [] } -> props: { children: any, items string [] }
@@ -8,7 +9,10 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        // <li key={item.id}>{item.text}</li>
+        // !!! we can add key={...} even though we don't specify it in TodoItem.tsx
+        // because it is of type React.FC !!!
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
